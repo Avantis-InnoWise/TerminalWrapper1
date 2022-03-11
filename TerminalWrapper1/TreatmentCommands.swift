dc0e5c1da01e570e22150e01828424eef5856723
Novik <imvladnovik@gmail.com>
2022-03-11 12:46:26 +0300

    Refactoring

--- TerminalWrapper1/TreatmentCommands.swift
+++ TerminalWrapper1/TreatmentCommands.swift
@@ -8,28 +8,29 @@
 import Foundation
 
 enum TreatmentCommands {
-    static let slt = "SomeSalt123456dgsdgqg"
-    static var sep = "FGG"
+    static let slt = "SomeSalt123456dgsdgqg" // Is a salt. We use it to mix with our original string.
+    static var sep = "FGG" // Is a separator. We use it to create string from array of numbers (UTF8), and separate encrypted string to original.
 
-    static func mix(str: String, slt: String) -> String {
-        let txt = [UInt8](str.utf8)
-        let cpr = [UInt8](slt.utf8)
+    // !!! We have to use the same salt and separator to encrypt and decrypt. If we change the parameter we should
+
+    // encrypting the string that we pass to the method
+    static func show(str: String, slt: String) -> String {
+        let txt = [UInt8](str.utf8) // original text in utf8
+        let cpr = [UInt8](slt.utf8) // salt in utf8
         var ecd = [UInt8]()
         for t in txt.enumerated() {
-            ecd.append(t.element ^ cpr[t.offset % cpr.count])
+            ecd.append(t.element ^ cpr[t.offset % cpr.count]) // mix original string and salt
         }
-        return ecd.map { String($0) }.joined(separator: TreatmentCommands.sep)
+        return ecd.map { String($0) }.joined(separator: TreatmentCommands.sep) // create string from array of numbers with separator
     }
 
     static func res(key: String, slt: String) -> String {
-        let cpr = [UInt8](slt.utf8)
+        let cpr = [UInt8](slt.utf8) // salt in utf8
         var dcd = [UInt8]()
         for k in key.components(separatedBy: TreatmentCommands.sep).map({ UInt8($0) ?? 0 }).enumerated() {
             dcd.append(k.element ^ cpr[k.offset % cpr.count])
-        }
+        } // decrypt original string
         return String(bytes: dcd, encoding: .utf8) ?? ""
     }
 
 }
-
-
